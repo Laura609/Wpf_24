bc6bb45aaf2fcb54739d77232e9f6bdce5d0e2f3
Laura <lavrusik515@gmail.com>
2023-12-27 09:33:15 +0300

    Добавьте файлы проекта.

---
+++ View/UserControls/UserControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+namespace Wpf_24.View.UserControls
+{
+    public partial class UserControl : UserControl
+    {
+        public static readonly DependencyProperty InputTextProperty =
+            DependencyProperty.Register("InputText", typeof(string), typeof(UserControl), new PropertyMetadata(""));
+
+        public string InputText
+        {
+            get { return (string)GetValue(InputTextProperty); }
+            set { SetValue(InputTextProperty, value); }
+        }
+
+
+        public event EventHandler EditButtonClicked;
+
+        public UserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void EditButtonClick(object sender, RoutedEventArgs e)
+        {
+            EditButtonClicked?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ View/Windows/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace Wpf_24
+{
+    public partial class MainWindow : Window
+    {
+        public string GraphicsData { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void EditButton_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
